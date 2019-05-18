b5b6cf2dd9ef31ec629ae29696912d7026ff5ad3
Lambe Boluwatife <40843759+lambeboluwatife@users.noreply.github.com>
2019-05-18 17:59:21 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,433 @@
+let staff = [
+  {
+    FirstName: "Vanessa",
+    SecondName: "Rogers",
+    Gender: "Female",
+    Age: 19,
+    Salary: 60193,
+  },
+  {
+    FirstName: "Charlotte",
+    SecondName: "Hunt",
+    Gender: "Female",
+    Age: 24,
+    Salary: 92383,
+  },
+  {
+    FirstName: "Marcus",
+    SecondName: "Foster",
+    Gender: "Male",
+    Age: 21,
+    Salary: 65760,
+  },
+  {
+    FirstName: "Myra",
+    SecondName: "Brooks",
+    Gender: "Female",
+    Age: 21,
+    Salary: 174036,
+  },
+  {
+    FirstName: "Caroline",
+    SecondName: "Armstrong",
+    Gender: "Female",
+    Age: 29,
+    Salary: 149879,
+  },
+  {
+    FirstName: "Belinda",
+    SecondName: "Carroll",
+    Gender: "Female",
+    Age: 19,
+    Salary: 188935,
+  },
+  {
+    FirstName: "Olivia",
+    SecondName: "Armstrong",
+    Gender: "Female",
+    Age: 26,
+    Salary: 34782,
+  },
+  {
+    FirstName: "April",
+    SecondName: "Cooper",
+    Gender: "Female",
+    Age: 20,
+    Salary: 68263,
+  },
+  {
+    FirstName: "Edith",
+    SecondName: "Harrison",
+    Gender: "Female",
+    Age: 23,
+    Salary: 54709,
+  },
+  {
+    FirstName: "Vivian",
+    SecondName: "Henderson",
+    Gender: "Female",
+    Age: 20,
+    Salary: 155709,
+  },
+  {
+    FirstName: "Ashton",
+    SecondName: "Martin",
+    Gender: "Male",
+    Age: 27,
+    Salary: 166647,
+  },
+  {
+    FirstName: "Gianna",
+    SecondName: "Moore",
+    Gender: "Female",
+    Age: 23,
+    Salary: 191619,
+  },
+  {
+    FirstName: "Camila",
+    SecondName: "Allen",
+    Gender: "Female",
+    Age: 23,
+    Salary: 85567,
+  },
+  {
+    FirstName: "Gianna",
+    SecondName: "Morrison",
+    Gender: "Female",
+    Age: 28,
+    Salary: 191509,
+  },
+  {
+    FirstName: "Paul",
+    SecondName: "Cooper",
+    Gender: "Male",
+    Age: 28,
+    Salary: 39471,
+  },
+  {
+    FirstName: "Tara",
+    SecondName: "Mason",
+    Gender: "Female",
+    Age: 19,
+    Salary: 189780,
+  },
+  {
+    FirstName: "Aida",
+    SecondName: "Tucker",
+    Gender: "Female",
+    Age: 21,
+    Salary: 143187,
+  },
+  {
+    FirstName: "Richard",
+    SecondName: "Cameron",
+    Gender: "Male",
+    Age: 21,
+    Salary: 116644,
+  },
+  {
+    FirstName: "Amy",
+    SecondName: "Stewart",
+    Gender: "Female",
+    Age: 29,
+    Salary: 126961,
+  },
+  {
+    FirstName: "Elenor",
+    SecondName: "Thompson",
+    Gender: "Female",
+    Age: 22,
+    Salary: 159003,
+  },
+  {
+    FirstName: "Alberta",
+    SecondName: "Edwards",
+    Gender: "Female",
+    Age: 21,
+    Salary: 105648,
+  },
+  {
+    FirstName: "Gianna",
+    SecondName: "Evans",
+    Gender: "Female",
+    Age: 27,
+    Salary: 183805,
+  },
+  {
+    FirstName: "Florrie",
+    SecondName: "Kelley",
+    Gender: "Female",
+    Age: 24,
+    Salary: 180568,
+  },
+  {
+    FirstName: "Miranda",
+    SecondName: "Harris",
+    Gender: "Female",
+    Age: 19,
+    Salary: 185753,
+  },
+  {
+    FirstName: "Henry",
+    SecondName: "Davis",
+    Gender: "Male",
+    Age: 18,
+    Salary: 83804,
+  },
+  {
+    FirstName: "Kirsten",
+    SecondName: "Barnes",
+    Gender: "Female",
+    Age: 26,
+    Salary: 161833,
+  },
+  {
+    FirstName: "Jack",
+    SecondName: "Chapman",
+    Gender: "Male",
+    Age: 19,
+    Salary: 41007,
+  },
+  {
+    FirstName: "Myra",
+    SecondName: "Kelly",
+    Gender: "Female",
+    Age: 27,
+    Salary: 118346,
+  },
+  {
+    FirstName: "Emma",
+    SecondName: "Evans",
+    Gender: "Female",
+    Age: 23,
+    Salary: 126125,
+  },
+  {
+    FirstName: "Blake",
+    SecondName: "Evans",
+    Gender: "Male",
+    Age: 24,
+    Salary: 69702,
+  },
+  {
+    FirstName: "Aldus",
+    SecondName: "Armstrong",
+    Gender: "Male",
+    Age: 26,
+    Salary: 132491,
+  },
+  {
+    FirstName: "Adelaide",
+    SecondName: "Martin",
+    Gender: "Female",
+    Age: 20,
+    Salary: 175263,
+  },
+  {
+    FirstName: "Victoria",
+    SecondName: "Elliott",
+    Gender: "Female",
+    Age: 21,
+    Salary: 111645,
+  },
+  {
+    FirstName: "Nicole",
+    SecondName: "Harrison",
+    Gender: "Female",
+    Age: 22,
+    Salary: 54624,
+  },
+  {
+    FirstName: "Edwin",
+    SecondName: "Hamilton",
+    Gender: "Male",
+    Age: 18,
+    Salary: 152586,
+  },
+  {
+    FirstName: "Jessica",
+    SecondName: "Robinson",
+    Gender: "Female",
+    Age: 29,
+    Salary: 85446,
+  },
+  {
+    FirstName: "Sofia",
+    SecondName: "Turner",
+    Gender: "Female",
+    Age: 20,
+    Salary: 131564,
+  },
+  {
+    FirstName: "Cadie",
+    SecondName: "Higgins",
+    Gender: "Female",
+    Age: 21,
+    Salary: 138663,
+  },
+  {
+    FirstName: "Jessica",
+    SecondName: "Turner",
+    Gender: "Female",
+    Age: 24,
+    Salary: 91319,
+  },
+  {
+    FirstName: "Walter",
+    SecondName: "Hamilton",
+    Gender: "Male",
+    Age: 25,
+    Salary: 117408,
+  },
+  {
+    FirstName: "Alexander",
+    SecondName: "Farrell",
+    Gender: "Male",
+    Age: 26,
+    Salary: 65878,
+  },
+  {
+    FirstName: "Justin",
+    SecondName: "Howard",
+    Gender: "Male",
+    Age: 21,
+    Salary: 165575,
+  },
+  {
+    FirstName: "Chelsea",
+    SecondName: "Holmes",
+    Gender: "Female",
+    Age: 21,
+    Salary: 32690,
+  },
+  {
+    FirstName: "Alberta",
+    SecondName: "Wilson",
+    Gender: "Female",
+    Age: 22,
+    Salary: 143459,
+  },
+  {
+    FirstName: "Miller",
+    SecondName: "Murphy",
+    Gender: "Male",
+    Age: 25,
+    Salary: 163775,
+  },
+  {
+    FirstName: "Adrianna",
+    SecondName: "Hawkins",
+    Gender: "Female",
+    Age: 29,
+    Salary: 64412,
+  },
+  {
+    FirstName: "Dominik",
+    SecondName: "Reed",
+    Gender: "Male",
+    Age: 23,
+    Salary: 42643,
+  },
+  {
+    FirstName: "Gianna",
+    SecondName: "Craig",
+    Gender: "Female",
+    Age: 18,
+    Salary: 141944,
+  },
+  {
+    FirstName: "Alissa",
+    SecondName: "Cameron",
+    Gender: "Female",
+    Age: 25,
+    Salary: 101581,
+  },
+  {
+    FirstName: "Aldus",
+    SecondName: "Adams",
+    Gender: "Male",
+    Age: 28,
+    Salary: 132894,
+  },
+]
+
+//ALL
+for (let i = 0; i < staff.length; i++) {
+  console.log(`
+     Name => ${staff[i].FirstName} ${staff[i].SecondName}\n
+     Gender => ${staff[i].Gender}\n
+     Age => ${staff[i].Age}\n
+     Salary => ${staff[i].Salary}\n
+  `);
+}
+
+//FIRSTNAME AND LASTNAME
+/*for (let i = 0; i < staff.length; i++) {
+  console.log(`${staff[i].FirstName} ${staff[i].SecondName}`);
+}*/
+
+//TOTAL SALARIES
+//let sum = 0;
+/*for (let i = 0; i < staff.length; i++) {
+  let value = staff[i].Salary;
+  sum = sum + value
+}
+console.log(sum);*/
+
+/*const newSalary = staff.Salary.reduce((acc, curr) => acc + curr, 0);
+console.log(newSalary);*/
+
+
+//PERSONAL INFO OF WORKKER AT INDEX 33
+//console.log(staff[33]);
+
+//SALARY OF SOFIA TURNER
+//console.log(`Salary of Sophia Turner is ${staff[36].Salary}`);
+for (let i = 0; i < staff.length; i++) {
+  if (staff[i].FirstName === "Sophia") {
+    console.log('Sophia');
+  }
+  else {
+    console.log("Okay");
+  }
+}
+
+//LENGTH OF THE ARRAY
+/*for (let i = 0; i < staff.length; i++) {
+  console.log(staff.length);
+}*/
+
+const bolu = (number, factor) => {
+  if (number % factor === 0 && typeof(number === 'number')) {
+    return `${factor} is a factor of ${number}`
+  }
+  else {
+    return `${factor} is not a factor of ${number}`
+  }
+}
+console.log(bolu(25, 5));
+
+const arrayofAges = [26, 56, 78, 9, 20, 44, 16, 10]
+
+//forin
+for (const name in arrayofAges) {
+  console.log(arrayofAges[name]);
+}
+
+//map()
+const agesafter5years = arrayofAges.map(age => age + 5)
+console.log(arrayofAges);
+console.log(agesafter5years);
+
+//Chaining
+arrayofAges.map(age => age + 5).forEach(age => console.log(age))
+
+//Filter
+const great = arrayofAges.filter(age => age < 18)
+console.log(great);
+const less = arrayofAges.filter(age => age > 18)
+console.log(less);
+
+//Reduce
+const sumofages = arrayofAges.reduce((acc, curr) => acc + curr, 0)
+console.log(sumofages);
